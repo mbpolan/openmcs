f27d30dfe7f6a755f2d91903dd9999504afc8af6
mbpolan <mike@mbpolan.com>
2023-04-22 13:20:00 -0400

    Add triggers to manage audit columns

--- migrations/sqlite3/01_init.up.sql
+++ migrations/sqlite3/01_init.up.sql
@@ -1,6 +1,3 @@
--- enable foreign keys
-PRAGMA foreign_keys = ON;
-
 -- create table for storing general player data
 CREATE TABLE PLAYER (
     -- primary key
@@ -42,6 +39,34 @@ CREATE TABLE PLAYER (
 -- create an index on player.username since it will be queried on login
 CREATE INDEX IDX_PLAYER_USERNAME ON PLAYER (USERNAME);
 
+-- create a trigger on player to manage the CREATED_DTTM column
+CREATE TRIGGER
+    PLAYER_CREATED_DTTM
+AFTER INSERT ON
+    PLAYER
+BEGIN
+    UPDATE
+        PLAYER
+    SET
+        CREATED_DTTM = DATETIME('NOW')
+    WHERE
+        ID = NEW.ID;
+END;
+
+-- create a trigger on player to manage the UPDATED_DTTM column
+CREATE TRIGGER
+    PLAYER_UPDATED_DTTM
+AFTER UPDATE ON
+    PLAYER
+BEGIN
+    UPDATE
+        PLAYER
+    SET
+        UPDATED_DTTM = DATETIME('NOW')
+    WHERE
+        ID = NEW.ID;
+END;
+
 -- create table for storing a player's equipped items
 CREATE TABLE PLAYER_EQUIPMENT (
     -- primary key
@@ -57,6 +82,37 @@ CREATE TABLE PLAYER_EQUIPMENT (
     -- date time when the row was updated
     UPDATED_DTTM TEXT NULL
 );
+
+-- create an index on player_equipment.player_id since it will be queried on
+CREATE INDEX IDX_PLAYER_EQUIPMENT_PLAYER_ID ON PLAYER_EQUIPMENT(PLAYER_ID);
+
+-- create a trigger on player_equipment to manage the CREATED_DTTM column
+CREATE TRIGGER
+    PLAYER_EQUIPMENT_CREATED_DTTM
+AFTER INSERT ON
+    PLAYER_EQUIPMENT
+BEGIN
+    UPDATE
+        PLAYER_EQUIPMENT
+    SET
+        CREATED_DTTM = DATETIME('NOW')
+    WHERE
+        ID = NEW.ID;
+END;
+
+-- create a trigger on player_equipment to manage the UPDATED_DTTM column
+CREATE TRIGGER
+    PLAYER_EQUIPMENT_UPDATED_DTTM
+AFTER UPDATE ON
+    PLAYER_EQUIPMENT
+BEGIN
+    UPDATE
+        PLAYER_EQUIPMENT
+    SET
+        UPDATED_DTTM = DATETIME('NOW')
+    WHERE
+        ID = NEW.ID;
+END;
 
 -- create table for storing a player's character appearance
 CREATE TABLE PLAYER_APPEARANCE (
@@ -74,6 +130,37 @@ CREATE TABLE PLAYER_APPEARANCE (
     UPDATED_DTTM TEXT NULL
 );
 
+-- create an index on player_appearance.player_id since it will be queried on
+CREATE INDEX IDX_PLAYER_APPEARANCE_PLAYER_ID ON PLAYER_APPEARANCE(PLAYER_ID);
+
+-- create a trigger on player_equipment to manage the CREATED_DTTM column
+CREATE TRIGGER
+    PLAYER_APPEARANCE_CREATED_DTTM
+AFTER INSERT ON
+    PLAYER_APPEARANCE
+BEGIN
+    UPDATE
+        PLAYER_APPEARANCE
+    SET
+        CREATED_DTTM = DATETIME('NOW')
+    WHERE
+        ID = NEW.ID;
+END;
+
+-- create a trigger on player_appearance to manage the UPDATED_DTTM column
+CREATE TRIGGER
+    PLAYER_APPEARANCE_UPDATED_DTTM
+AFTER UPDATE ON
+    PLAYER_APPEARANCE
+BEGIN
+    UPDATE
+        PLAYER_APPEARANCE
+    SET
+        UPDATED_DTTM = DATETIME('NOW')
+    WHERE
+        ID = NEW.ID;
+END;
+
 -- create table for storing player friends lists
 CREATE TABLE FRIEND (
     -- primary key
@@ -88,6 +175,37 @@ CREATE TABLE FRIEND (
     UPDATED_DTTM TEXT NULL
 );
 
+-- create an index on friend.player_id since it will be queried on
+CREATE INDEX IDX_FRIEND_PLAYER_ID ON FRIEND(PLAYER_ID);
+
+-- create a trigger on friend to manage the CREATED_DTTM column
+CREATE TRIGGER
+    FRIEND_CREATED_DTTM
+AFTER INSERT ON
+    FRIEND
+BEGIN
+    UPDATE
+        FRIEND
+    SET
+        CREATED_DTTM = DATETIME('NOW')
+    WHERE
+        ID = NEW.ID;
+END;
+
+-- create a trigger on friend to manage the UPDATED_DTTM column
+CREATE TRIGGER
+    FRIEND_UPDATED_DTTM
+AFTER UPDATE ON
+    FRIEND
+BEGIN
+    UPDATE
+        FRIEND
+    SET
+        UPDATED_DTTM = DATETIME('NOW')
+    WHERE
+        ID = NEW.ID;
+END;
+
 -- create table for storing player ignore lists
 CREATE TABLE IGNORED (
     -- primary key
@@ -101,3 +219,34 @@ CREATE TABLE IGNORED (
     -- date time when the row was updated
     UPDATED_DTTM TEXT NULL
 );
+
+-- create an index on ignored.player_id since it will be queried on
+CREATE INDEX IDX_IGNORED_PLAYER_ID ON IGNORED(PLAYER_ID);
+
+-- create a trigger on ignored to manage the CREATED_DTTM column
+CREATE TRIGGER
+    IGNORED_CREATED_DTTM
+AFTER INSERT ON
+    IGNORED
+BEGIN
+    UPDATE
+        IGNORED
+    SET
+        CREATED_DTTM = DATETIME('NOW')
+    WHERE
+        ID = NEW.ID;
+END;
+
+-- create a trigger on ignored to manage the UPDATED_DTTM column
+CREATE TRIGGER
+    IGNORED_UPDATED_DTTM
+AFTER UPDATE ON
+    IGNORED
+BEGIN
+    UPDATE
+        IGNORED
+    SET
+        UPDATED_DTTM = DATETIME('NOW')
+    WHERE
+        ID = NEW.ID;
+END;
