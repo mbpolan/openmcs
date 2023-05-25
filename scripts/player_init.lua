30b078d3252ec139c9716b2527f053c0c9ba50e2
mbpolan <mike@mbpolan.com>
2023-05-24 20:21:21 -0400

    Send interface for standard spell book

--- scripts/player_init.lua
+++ scripts/player_init.lua
@@ -9,6 +9,7 @@ function init_player_tabs(player)
     player:sidebar_interface(CLIENT_TAB_LOGOUT, 2449)
     player:sidebar_interface(CLIENT_TAB_FRIENDS_LIST, 5065)
     player:sidebar_interface(CLIENT_TAB_IGNORE_LIST, 5715)
+    player:sidebar_interface(CLIENT_TAB_SPELLS, 1151)
 
     -- set the equipped item interface based on the currently equipped weapon
     item = player:equipped_item(EQUIP_SLOT_WEAPON)
@@ -22,7 +23,6 @@ function init_player_tabs(player)
     -- TODO: not yet supported by game engine
     player:sidebar_clear(CLIENT_TAB_QUESTS)
     player:sidebar_clear(CLIENT_TAB_PRAYERS)
-    player:sidebar_clear(CLIENT_TAB_SPELLS)
     player:sidebar_clear(CLIENT_TAB_SETTINGS)
     player:sidebar_clear(CLIENT_TAB_CONTROLS)
     player:sidebar_clear(CLIENT_TAB_MUSIC)
