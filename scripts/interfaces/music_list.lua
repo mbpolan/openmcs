220941cd6128fea18924c950f66260e952f5f3dc
mbpolan <mike@mbpolan.com>
2023-06-06 20:16:39 -0400

    Validate if player has music track unlocked before playing it

--- scripts/interfaces/music_list.lua
+++ scripts/interfaces/music_list.lua
@@ -3,6 +3,11 @@ MUSIC_IDS_TO_INTERFACES = {
     [151] = 13972
 }
 
+-- map of music identifiers to flags if they are available without unlock
+DEFAULT_MUSIC_IDS = {
+    [151] = true
+}
+
 -- map of music text interfaces to music identifiers
 MUSIC_INTERFACES_TO_IDS = {}
 for k, v in pairs(MUSIC_IDS_TO_INTERFACES) do
@@ -23,16 +28,24 @@ function interface_962_on_action(player, interface)
         -- loop mode
     end
 
-    -- play a selected song
+    -- find the song id that was clicked
     local song_id = MUSIC_INTERFACES_TO_IDS[id]
     if song_id == nil then
         player:server_message("This music track is not yet available!")
         return
     end
 
-    -- play the song
-    -- TODO: check if the player has this track unlocked, if it's not a default track
-    player:play_music(song_id)
+    -- play the song if the player has this track unlocked of if it's available by default
+    local available = DEFAULT_MUSIC_IDS[song_id]
+    if not available then
+        available = player:music_track(song_id)
+    end
+
+    if available then
+        player:play_music(song_id)
+    else
+        player:server_message("You need to unlock this music track first.")
+    end
 end
 
 --- Handles updating the music list interface.
