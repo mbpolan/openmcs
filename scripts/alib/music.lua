3bcd8177433aee3e4b7fbf3bac0305539218e83e
mbpolan <mike@mbpolan.com>
2023-06-06 20:33:37 -0400

    Update music list interface with unlocked tracks

---
+++ scripts/alib/music.lua
@@ -0,0 +1,7 @@
+-------------------------------------
+-- Music and Sound
+-------------------------------------
+
+-- identifiers for music tracks
+MUSIC_MEDIEVAL = 82
+MUSIC_GRND_SCP = 151
--- scripts/interfaces/music_list.lua
+++ scripts/interfaces/music_list.lua
@@ -1,11 +1,12 @@
 -- map of music identifiers for interface text
 MUSIC_IDS_TO_INTERFACES = {
-    [151] = 13972
+    [MUSIC_MEDIEVAL] = 4368,
+    [MUSIC_GRND_SCP] = 13972
 }
 
 -- map of music identifiers to flags if they are available without unlock
 DEFAULT_MUSIC_IDS = {
-    [151] = true
+    [MUSIC_GRND_SCP] = true
 }
 
 -- map of music text interfaces to music identifiers
@@ -28,21 +29,21 @@ function interface_962_on_action(player, interface)
         -- loop mode
     end
 
-    -- find the song id that was clicked
-    local song_id = MUSIC_INTERFACES_TO_IDS[id]
-    if song_id == nil then
+    -- find the music track id that was clicked
+    local music_id = MUSIC_INTERFACES_TO_IDS[id]
+    if music_id == nil then
         player:server_message("This music track is not yet available!")
         return
     end
 
-    -- play the song if the player has this track unlocked of if it's available by default
-    local available = DEFAULT_MUSIC_IDS[song_id]
+    -- play the track if the player has this track unlocked of if it's available by default
+    local available = DEFAULT_MUSIC_IDS[music_id]
     if not available then
-        available = player:music_track(song_id)
+        available = player:music_track(music_id)
     end
 
     if available then
-        player:play_music(song_id)
+        player:play_music(music_id)
     else
         player:server_message("You need to unlock this music track first.")
     end
@@ -51,5 +52,14 @@ end
 --- Handles updating the music list interface.
 -- @param player The player
 function interface_962_on_update(player)
-    -- TODO
+    -- if a non-default track is unlocked, update its interface color
+    for music_id, inf_id in pairs(MUSIC_IDS_TO_INTERFACES) do
+        if DEFAULT_MUSIC_IDS[music_id] ~= true then
+            if player:music_track(music_id) then
+                player:interface_color(inf_id, 0, 31, 0)
+            else
+                player:interface_color(inf_id, 31, 0, 0)
+            end
+        end
+    end
 end
